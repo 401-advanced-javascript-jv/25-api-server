6bc7b4f6d5d29683648d812ecd856b8a2073053e
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-16 12:28:39 -0700

    add qclient middleware and badly-formatted jsdocs

---
+++ src/middleware/qclient.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @module QClient Logger
+ * Sends database events to the QClient logger
+ */
+const QClient = require('@nmq/q/client');
+
+module.exports = (request, response, next) => {
+  let method = request.method.toLowerCase();
+  let url = request.url;
+  QClient.publish('database', method, {method, url});
+  next();
+}
